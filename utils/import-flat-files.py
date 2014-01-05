c8aaa90d53003f584eca1b64db0b9f7f7b52ad04
shapiromatron <shapiromatron@gmail.com>
2014-01-05 17:05:59 -0500

    Added error catching and creation of logfile; also fixed but with fixed timestep instead of variable timestep being added.

--- utils/import-flat-files.py
+++ utils/import-flat-files.py
@@ -1,19 +1,23 @@
 import argparse
+from collections import OrderedDict
+import datetime
 import os
 import sys
 
-RAW_DATA_DIR = '../raw-data'
-sys.path.append(os.path.abspath('.'))
-
-from collections import OrderedDict
-from datetime import datetime
+sys.path.append(os.path.abspath('.')) # require for imports below
 
 from django.conf import settings
+from django.utils.timezone import get_default_timezone, make_aware
 from stops.models import Stop, Person, Search, Contraband, SearchBasis
 
 
+RAW_DATA_DIR = '../raw-data'
+logfn = os.path.join(RAW_DATA_DIR, 'import-log.txt')
+tz = get_default_timezone()
+
+
 def _date_converter(txt):
-    return datetime.strptime(r'2000-01-03 20:35:00.000', '%Y-%m-%d %H:%M:%S.%f')
+    return make_aware(datetime.datetime.strptime(txt, '%Y-%m-%d %H:%M:%S.%f'), tz)
 
 
 def _float_converter(txt):
@@ -23,11 +27,17 @@ def _float_converter(txt):
         return 0.
 
 
+def log_error(class_type, row_index, txt):
+    with file(logfn, 'a') as log:
+        log.write('{ct} row {i} not saved: {txt}\n'.format(ct=class_type, i=row_index, txt=txt))
+
+
 class StopConverter(object):
 
     django_class = Stop
 
-    def __init__(self, txt):
+    def __init__(self, txt, index):
+        self.index = index
         self.raw = OrderedDict([("StopID", txt[0:12].strip()),
                                 ("AgencyDescription", txt[12:112].strip()),
                                 ("StopDate", txt[112:136].strip()),
@@ -45,29 +55,34 @@ class StopConverter(object):
                                 ("StopCity", txt[192:212].strip())])
 
     def get_django_object(self):
-        obj = Stop(stop_id = int(self.raw["StopID"]),
-                   agency_description = self.raw["AgencyDescription"],
-                   date = _date_converter(self.raw["StopDate"]),
-                   purpose = int(self.raw["Purpose"]),
-                   action = int(self.raw["Action"]),
-                   driver_arrest = bool(int(self.raw["DriverArrest"])),
-                   passenger_arrest = bool(int(self.raw["PassengerArrest"])),
-                   encounter_force = bool(int(self.raw["EncounterForce"])),
-                   engage_force = bool(int(self.raw["EngageForce"])),
-                   officer_injury = bool(int(self.raw["OfficerInjury"])),
-                   driver_injury = bool(int(self.raw["DriverInjury"])),
-                   passenger_injury = bool(int(self.raw["PassengerInjury"])),
-                   officer_id = self.raw["OfficerId"],
-                   stop_location = self.raw["StopLocation"],
-                   stop_city = self.raw["StopCity"])
-        return obj
+        try:
+            obj = Stop(stop_id = int(self.raw["StopID"]),
+                       agency_description = self.raw["AgencyDescription"],
+                       date = _date_converter(self.raw["StopDate"]),
+                       purpose = int(self.raw["Purpose"]),
+                       action = int(self.raw["Action"]),
+                       driver_arrest = bool(int(self.raw["DriverArrest"])),
+                       passenger_arrest = bool(int(self.raw["PassengerArrest"])),
+                       encounter_force = bool(int(self.raw["EncounterForce"])),
+                       engage_force = bool(int(self.raw["EngageForce"])),
+                       officer_injury = bool(int(self.raw["OfficerInjury"])),
+                       driver_injury = bool(int(self.raw["DriverInjury"])),
+                       passenger_injury = bool(int(self.raw["PassengerInjury"])),
+                       officer_id = self.raw["OfficerId"],
+                       stop_location = self.raw["StopLocation"],
+                       stop_city = self.raw["StopCity"])
+            return obj
+        except:
+            log_error("Stop", self.index, "")
+            return None
 
 
 class PersonConverter(object):
 
     django_class = Person
 
-    def __init__(self, txt):
+    def __init__(self, txt, index):
+        self.index = index
         self.raw = OrderedDict([("PersonID", txt[0:12].strip()),
                                 ("StopID", txt[12:24].strip()),
                                 ("Type", txt[24:25].strip()),
@@ -77,21 +92,27 @@ class PersonConverter(object):
                                 ("Race", txt[32:33].strip())])
 
     def get_django_object(self):
-        obj = Person(person_id = int(self.raw["PersonID"]),
-                     stop = Stop.objects.get(pk=int(self.raw["StopID"])),
-                     type = self.raw["Type"],
-                     age = self.raw["Age"],
-                     gender = self.raw["Gender"],
-                     ethnicity = self.raw["Ethnicity"],
-                     race = self.raw["Race"])
-        return obj
+        try:
+            obj = Person(person_id = int(self.raw["PersonID"]),
+                         stop = Stop.objects.get(pk=int(self.raw["StopID"])),
+                         type = self.raw["Type"],
+                         age = self.raw["Age"],
+                         gender = self.raw["Gender"],
+                         ethnicity = self.raw["Ethnicity"],
+                         race = self.raw["Race"])
+            return obj
+        except:
+            log_error("Person", self.index,
+                      "stop_id = {s}".format(s=self.raw["StopID"]))
+            return None
 
 
 class SearchConverter(object):
 
     django_class = Search
 
-    def __init__(self, txt):
+    def __init__(self, txt, index):
+        self.index = index
         self.raw = OrderedDict([("SearchID", txt[0:12].strip()),
                                 ("StopID", txt[12:24].strip()),
                                 ("PersonID", txt[24:36].strip()),
@@ -105,25 +126,32 @@ class SearchConverter(object):
                                 ("OtherPropertySeized", txt[59:62].strip())])
 
     def get_django_object(self):
-        obj = Search(search_id = int(self.raw["SearchID"]),
-                     stop= Stop.objects.get(pk=int(self.raw["StopID"])),
-                     person= Person.objects.get(pk=int(self.raw["PersonID"])),
-                     type = self.raw["Type"],
-                     vehicle_search=bool(int(self.raw["VehicleSearch"])),
-                     driver_search=bool(int(self.raw["DriverSearch"])),
-                     passenger_search=bool(int(self.raw["PassengerSearch"])),
-                     property_search=bool(int(self.raw["PropertySearch"])),
-                     vehicle_siezed=bool(int(self.raw["VehicleSeized"])),
-                     personal_property_siezed=bool(int(self.raw["PersonalPropertySeized"])),
-                     other_property_sized=bool(int(self.raw["OtherPropertySeized"])))
-        return obj
+        try:
+            obj = Search(search_id = int(self.raw["SearchID"]),
+                         stop= Stop.objects.get(pk=int(self.raw["StopID"])),
+                         person= Person.objects.get(pk=int(self.raw["PersonID"])),
+                         type = self.raw["Type"],
+                         vehicle_search=bool(int(self.raw["VehicleSearch"])),
+                         driver_search=bool(int(self.raw["DriverSearch"])),
+                         passenger_search=bool(int(self.raw["PassengerSearch"])),
+                         property_search=bool(int(self.raw["PropertySearch"])),
+                         vehicle_siezed=bool(int(self.raw["VehicleSeized"])),
+                         personal_property_siezed=bool(int(self.raw["PersonalPropertySeized"])),
+                         other_property_sized=bool(int(self.raw["OtherPropertySeized"])))
+            return obj
+        except:
+            log_error("Search", self.index,
+                      "stop_id = {s}, person_id={p}".format(s=self.raw["StopID"],
+                                                            p=self.raw["PersonID"]))
+            return None
 
 
 class ContrabandConverter(object):
 
     django_class = Contraband
 
-    def __init__(self, txt):
+    def __init__(self, txt, index):
+        self.index = index
         self.raw = OrderedDict([("ContrabandID", txt[0:12].strip()),
                                 ("SearchID", txt[12:24].strip()),
                                 ("PersonID", txt[24:36].strip()),
@@ -140,28 +168,36 @@ class ContrabandConverter(object):
                                 ("DollarAmt", txt[417:458].strip())])
 
     def get_django_object(self):
-        obj = Contraband(contraband_id=self.raw["ContrabandID"],
-                         search=Search.objects.get(pk=self.raw["SearchID"]),
-                         person=Person.objects.get(pk=self.raw["PersonID"]),
-                         stop=Stop.objects.get(pk=self.raw["StopID"]),
-                         ounces=_float_converter(self.raw["Ounces"]),
-                         pounds=_float_converter(self.raw["Pounds"]),
-                         pints=_float_converter(self.raw["Pints"]),
-                         gallons=_float_converter(self.raw["Gallons"]),
-                         dosages=_float_converter(self.raw["Dosages"]),
-                         grams=_float_converter(self.raw["Grams"]),
-                         kilos=_float_converter(self.raw["Kilos"]),
-                         money=_float_converter(self.raw["Money"]),
-                         weapons=_float_converter(self.raw["Weapons"]),
-                         dollar_amount=_float_converter(self.raw["DollarAmt"]))
-        return obj
+        try:
+            obj = Contraband(contraband_id=self.raw["ContrabandID"],
+                             search=Search.objects.get(pk=self.raw["SearchID"]),
+                             person=Person.objects.get(pk=self.raw["PersonID"]),
+                             stop=Stop.objects.get(pk=self.raw["StopID"]),
+                             ounces=_float_converter(self.raw["Ounces"]),
+                             pounds=_float_converter(self.raw["Pounds"]),
+                             pints=_float_converter(self.raw["Pints"]),
+                             gallons=_float_converter(self.raw["Gallons"]),
+                             dosages=_float_converter(self.raw["Dosages"]),
+                             grams=_float_converter(self.raw["Grams"]),
+                             kilos=_float_converter(self.raw["Kilos"]),
+                             money=_float_converter(self.raw["Money"]),
+                             weapons=_float_converter(self.raw["Weapons"]),
+                             dollar_amount=_float_converter(self.raw["DollarAmt"]))
+            return obj
+        except:
+            log_error("Contraband", self.index,
+                      "stop_id = {s}, search_id={se}, person_id={p}".format(s=self.raw["StopID"],
+                                                                            se=self.raw["SearchID"],
+                                                                            p=self.raw["PersonID"]))
+            return None
 
 
 class SearchBasisConverter(object):
 
     django_class = SearchBasis
 
-    def __init__(self, txt):
+    def __init__(self, txt, index):
+        self.index = index
         self.raw = OrderedDict([("SearchBasisID", txt[0:12].strip()),
                                 ("SearchID", txt[12:24].strip()),
                                 ("PersonID", txt[24:36].strip()),
@@ -169,12 +205,19 @@ class SearchBasisConverter(object):
                                 ("Basis", txt[48:58].strip())])
 
     def get_django_object(self):
-        obj = SearchBasis(search_basis_id=self.raw["SearchBasisID"],
-                          search=Search.objects.get(pk=self.raw["SearchID"]),
-                          person=Person.objects.get(pk=self.raw["PersonID"]),
-                          stop=Stop.objects.get(pk=self.raw["StopID"]),
-                          basis=self.raw["Basis"])
-        return obj
+        try:
+            obj = SearchBasis(search_basis_id=self.raw["SearchBasisID"],
+                              search=Search.objects.get(pk=self.raw["SearchID"]),
+                              person=Person.objects.get(pk=self.raw["PersonID"]),
+                              stop=Stop.objects.get(pk=self.raw["StopID"]),
+                              basis=self.raw["Basis"])
+            return obj
+        except:
+            log_error("SearchBasis", self.index,
+                      "stop_id = {s}, search_id={se}, person_id={p}".format(s=self.raw["StopID"],
+                                                                            se=self.raw["SearchID"],
+                                                                            p=self.raw["PersonID"]))
+            return None
 
 
 def load_files(fn, converter_class, start_line, number_lines, save_interval=10000):
@@ -186,8 +229,10 @@ def load_files(fn, converter_class, start_line, number_lines, save_interval=1000
                 break
             if i>=start_line:
                 #encoding: SQL_Latin1_General_CP1_CI_AS
-                conv = converter_class(line.decode('latin-1').encode('utf-8'))
-                objects.append(conv.get_django_object())
+                conv = converter_class(line.decode('latin-1').encode('utf-8'), i)
+                obj = conv.get_django_object()
+                if obj:
+                    objects.append(obj)
                 if i%save_interval==0:
                     print 'Saving rows {i}'.format(i=i)
                     converter_class.django_class.objects.bulk_create(objects)
@@ -196,6 +241,7 @@ def load_files(fn, converter_class, start_line, number_lines, save_interval=1000
     # save final objects at the end of the loop
     converter_class.django_class.objects.bulk_create(objects)
     print 'Objects {s} to {e} successfully loaded'.format(s=start_line, e=i-1)
+
 
 def main():
 
@@ -225,6 +271,13 @@ def main():
     else:
         print 'Unknown file-type specified for import'
         return
+
+    with file(logfn, 'a') as log:
+        now = datetime.datetime.now().strftime("%A, %b %d %Y at %I:%m %p")
+        log.write('Import log notes on {now}\n'.format(now=now))
+        log.write('File Type: {ft} | Start Line: {sl} | Number Lines: {nl}\n'.format(ft=args.file_type,
+                                                                                     sl=args.start_line,
+                                                                                     nl=args.number_lines))
 
     load_files(fn, conv, args.start_line, args.number_lines, args.lps)
 
