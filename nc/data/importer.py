b59736670302aaf19bd3fba76bc2285e386c2c19
Colin Copeland <copelco@caktusgroup.com>
2015-08-02 16:38:43 -0400

    fix noqa comment on SQL lines

--- nc/data/importer.py
+++ nc/data/importer.py
@@ -134,20 +134,20 @@ def copy_from(destination):
 # Adapted from: http://blog.hagander.net/archives/131-Automatically-dropping-and-creating-constraints.html    # noqa
 
 
-SELECT_DROP_CONSTRAINTS_SQL = """  # noqa
+SELECT_DROP_CONSTRAINTS_SQL = """
 SELECT 'ALTER TABLE "'||nspname||'"."'||relname||'" DROP CONSTRAINT IF EXISTS "'||conname||'";'
 FROM pg_constraint
 INNER JOIN pg_class ON conrelid=pg_class.oid
 INNER JOIN pg_namespace ON pg_namespace.oid=pg_class.relnamespace
 WHERE relname LIKE 'nc_%'
 ORDER BY CASE WHEN contype='f' THEN 0 ELSE 1 END,contype,nspname,relname,conname;
-"""
+"""  # noqa
 
-SELECT_ADD_CONSTRAINTS_SQL = """  # noqa
+SELECT_ADD_CONSTRAINTS_SQL = """
 SELECT 'ALTER TABLE "'||nspname||'"."'||relname||'" ADD CONSTRAINT "'||conname||'" '||pg_get_constraintdef(pg_constraint.oid)||';'
 FROM pg_constraint
 INNER JOIN pg_class ON conrelid=pg_class.oid
 INNER JOIN pg_namespace ON pg_namespace.oid=pg_class.relnamespace
 WHERE relname LIKE 'nc_%'
 ORDER BY CASE WHEN contype='f' THEN 0 ELSE 1 END DESC,contype DESC,nspname DESC,relname DESC,conname DESC;
-"""
+"""  # noqa
