aae08187ba752b2604a49a7a0147325551691dc2
Andy Shapiro <shapiromatron@gmail.com>
2015-09-17 00:13:49 +0200

    load views module instead of multiple fn from that module

--- traffic_stops/urls.py
+++ traffic_stops/urls.py
@@ -3,17 +3,17 @@ from django.conf.urls import patterns, include, url
 from django.conf.urls.static import static
 from django.contrib import admin
 
-from nc.views import home, UpdateSession
+from nc import views
 
 
 admin.autodiscover()
 
 
 urlpatterns = patterns('',  # noqa
-    url(r'^$', home, name='home'),
+    url(r'^$', views.home, name='home'),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^selectable/', include('selectable.urls')),
     url(r'^', include('nc.urls')),
-    url(r'^update-session/', UpdateSession.as_view(), name='update_session'),
+    url(r'^update-session/', views.UpdateSession.as_view(), name='update_session'),
     (r'^robots\.txt$', include('robots.urls')),
 ) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
