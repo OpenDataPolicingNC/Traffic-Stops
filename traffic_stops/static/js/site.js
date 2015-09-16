829fcb9942b354de3219565cc24e1b45c59384c7
Andy Shapiro <shapiromatron@gmail.com>
2015-09-17 00:11:43 +0200

    fix contraband table to show n_contraband/n_count instead of just n_contraband

--- traffic_stops/static/js/site.js
+++ traffic_stops/static/js/site.js
@@ -1172,18 +1172,27 @@ UseOfForceTable = StopsTable.extend({});
 
 ContrabandTable = TableBase.extend({
   get_tabular_data: function(){
-    var header, row, rows = [];
+    var header, row, rows = [], se, cb;
 
     // create header
     header = ["Year"];
     header.push.apply(header, Stops.pprint.values());
     rows.push(header);
 
-    // create data rows
-    this.data.raw.contraband.forEach(function(v){
-      row = [v.year];
-      Stops.races.forEach(function(r){ row.push(v[r]||0).toLocaleString(); });
-      Stops.ethnicities.forEach(function(e){ row.push(v[e]||0).toLocaleString(); });
+    var raw = this.data.raw,
+        searches = _.object(_.pluck(raw.searches, 'year'), raw.searches),
+        contrabands = _.object(_.pluck(raw.contraband, 'year'), raw.contraband);
+
+    _.keys(searches).forEach(function(yr){
+      se = searches[yr];
+      cb = contrabands[yr] || {};
+      row = [yr];
+      Stops.races.forEach(function(r){
+        row.push((cb[r]||0).toLocaleString() + "/" + (se[r]||0).toLocaleString());
+      });
+      Stops.ethnicities.forEach(function(e){
+        row.push((cb[e]||0).toLocaleString() + "/" + (se[e]||0).toLocaleString());
+      });
       rows.push(row);
     });
 
