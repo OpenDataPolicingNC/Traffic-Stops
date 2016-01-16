d23f3bb8e8415e20a66209472f277ebabfd98dbc
Colin Copeland <copelco@caktusgroup.com>
2016-01-16 13:32:22 -0500

    load JS modules in site.js

--- traffic_stops/static/js/index.js
+++ traffic_stops/static/js/index.js
@@ -1,7 +1,8 @@
-import $ from 'jquery'
-/* ... additional imports ... */
-
-window.jQuery = window.$ = require('jquery')
+window.jQuery = window.$ = require('jquery');
+window._ = require('underscore');
+window.Backbone = require('backbone');
+window.Backbone.$ = $;
+window.d3 = require('d3');
+var nv = require('nvd3');
 
 import './csrf_ajax.js'
-import './site.js'
--- traffic_stops/static/js/site.js
+++ traffic_stops/static/js/site.js
@@ -1,10 +1,3 @@
-var _ = require('underscore');
-var Backbone = require('backbone');
-var $ = require('jquery');
-Backbone.$ = $;
-import 'd3';
-import 'nvd3';
-
 // usage: log('inside coolFunc', this, arguments);
 // paulirish.com/2009/log-a-lightweight-wrapper-for-consolelog/
 window.log = function f(){ log.history = log.history || []; log.history.push(arguments); if(this.console) { var args = arguments, newarr; try { args.callee = f.caller } catch(e) {}; newarr = [].slice.call(args); if (typeof console.log === 'object') log.apply.call(console.log, console, newarr); else console.log.apply(console, newarr);}};
@@ -1246,5 +1239,3 @@ _.extend(RaceToggle.prototype, {
     });
   }
 });
-
-console.log(window.agency_id);
