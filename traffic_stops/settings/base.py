40998a1955bdb9a2298e8e4e98556683642b2b91
Colin Copeland <copelco@caktusgroup.com>
2016-04-24 12:31:43 -0400

    only set SYSLOG_PATH if exists (and hopefully fix travis)

--- traffic_stops/settings/base.py
+++ traffic_stops/settings/base.py
@@ -164,10 +164,10 @@ INSTALLED_APPS = (
 # the site admins on every HTTP 500 error when DEBUG=False.
 # See http://docs.djangoproject.com/en/dev/topics/logging for
 # more details on how to customize your logging configuration.
-if os.path.exists("/dev/log"):
-    SYSLOG_PATH = "/dev/log"
-else:
-    SYSLOG_PATH = "/var/run/syslog"
+SYSLOG_PATH = None
+for path in ("/dev/log", "/var/run/syslog"):
+    if os.path.exists(path):
+        SYSLOG_PATH = path
 
 LOGGING = {
     'version': 1,
