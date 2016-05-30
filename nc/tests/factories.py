b28c9ddfc82cbbf059bdf7fa748bc370fcffe562
Colin Copeland <copelco@caktusgroup.com>
2016-05-29 20:29:11 -0400

    add test for searches; refactor factories to not over-create sub/related factories

--- nc/tests/factories.py
+++ nc/tests/factories.py
@@ -22,6 +22,8 @@ class PersonFactory(factory.django.DjangoModelFactory):
     stop = factory.SubFactory('nc.tests.factories.StopFactory')
     age = factory.fuzzy.FuzzyInteger(16, 100)
     race = factory.fuzzy.FuzzyChoice(x[0] for x in models.RACE_CHOICES)
+    ethnicity = factory.fuzzy.FuzzyChoice(
+        x[0] for x in models.ETHNICITY_CHOICES)
     type = 'D'
 
 
@@ -35,7 +37,7 @@ class StopFactory(factory.django.DjangoModelFactory):
     date = factory.fuzzy.FuzzyDate(datetime.date(2008, 1, 1))
     purpose = factory.fuzzy.FuzzyChoice(x[0] for x in models.PURPOSE_CHOICES)
     action = factory.fuzzy.FuzzyChoice(x[0] for x in models.ACTION_CHOICES)
-    person = factory.RelatedFactory(PersonFactory, 'stop')
+    officer_id = factory.fuzzy.FuzzyInteger(0)
 
     @factory.post_generation
     def year(self, create, extracted, **kwargs):
@@ -44,3 +46,14 @@ class StopFactory(factory.django.DjangoModelFactory):
             return
         if extracted:
             self.date = self.date.replace(year=extracted)
+
+
+class SearchFactory(factory.django.DjangoModelFactory):
+
+    class Meta(object):
+        model = models.Search
+
+    search_id = factory.Sequence(lambda x: x)
+    stop = factory.SubFactory(StopFactory)
+    person = factory.SubFactory(PersonFactory)
+    type = factory.fuzzy.FuzzyChoice(x[0] for x in models.SEARCH_TYPE_CHOICES)
--- nc/tests/test_api.py
+++ nc/tests/test_api.py
@@ -2,6 +2,7 @@ from django.core.urlresolvers import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
 from nc.tests import factories
+from nc.api import GROUPS
 
 
 class AgencyTests(APITestCase):
@@ -25,12 +26,12 @@ class AgencyTests(APITestCase):
     def test_stops_count(self):
         """Test Agency stop counts"""
         agency = factories.AgencyFactory()
-        factories.StopFactory(person__race='B', agency=agency,
-                              person__ethnicity='N', year=2010)
-        factories.StopFactory(person__race='B', agency=agency,
-                              person__ethnicity='N', year=2010)
-        factories.StopFactory(person__race='W', agency=agency,
-                              person__ethnicity='N', year=2012)
+        factories.PersonFactory(race='B', stop__agency=agency,
+                                ethnicity='N', stop__year=2010)
+        factories.PersonFactory(race='B', stop__agency=agency,
+                                ethnicity='N', stop__year=2010)
+        factories.PersonFactory(race='W', stop__agency=agency,
+                                ethnicity='N', stop__year=2012)
         url = reverse('agency-api-stops', args=[agency.pk])
         response = self.client.get(url, format='json')
         self.assertEqual(response.data[0]['year'], 2010)
@@ -38,9 +39,29 @@ class AgencyTests(APITestCase):
         self.assertEqual(response.data[1]['year'], 2012)
         self.assertEqual(response.data[1]['white'], 1)
 
+    def test_officer_stops_count(self):
+        """Test officer (within an agency) stop counts"""
+        p = factories.PersonFactory()
+        url = reverse('agency-api-stops', args=[p.stop.agency.pk])
+        url = "{}?officer={}".format(url, p.stop.officer_id)
+        response = self.client.get(url, format='json')
+        self.assertEqual(response.data[0]['year'], p.stop.date.year)
+        self.assertEqual(response.data[0][GROUPS[p.race]], 1)
+
     def test_stops_by_reason(self):
         """Test Agency stops_by_reason API endpoint"""
         agency = factories.AgencyFactory()
         url = reverse('agency-api-stops-by-reason', args=[agency.pk])
         response = self.client.get(url, format='json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_searches(self):
+        """Test Agency search counts"""
+        agency = factories.AgencyFactory()
+        p1 = factories.PersonFactory(stop__agency=agency)
+        s1 = factories.SearchFactory(person=p1, stop=p1.stop)
+        url = reverse('agency-api-searches', args=[agency.pk])
+        response = self.client.get(url, format='json')
+        self.assertEqual(len(response.data), 1)
+        self.assertEqual(response.data[0]['year'], s1.stop.date.year)
+        self.assertEqual(response.data[0][GROUPS.get(s1.person.race)], 1)
