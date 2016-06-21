c848a5a1d94da7919b3272e9e0ee9748091ba04a
Jeff Trawick <trawick@gmail.com>
2016-06-21 12:51:48 -0400

    Fix URL to current MD dataset on S3

--- md/data/__init__.py
+++ md/data/__init__.py
@@ -1,3 +1,3 @@
-DEFAULT_URL = "https://s3-us-west-2.amazonaws.com/openpolicingdata/Maryland-Traffic-Stop-Data-2013.zip"  # noqa
+DEFAULT_URL = 'https://s3-us-west-2.amazonaws.com/openpolicingdata/PIALog_16-0806.zip'  # noqa
 DATASET_BASENAME = 'PIALog_16-0806'
 # DATASET_BASENAME = 'Small-0806'
