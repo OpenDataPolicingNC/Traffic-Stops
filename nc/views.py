a15a5b79c63509c47e327dd5eda91d7688f4d932
Rebecca Conley <rebecca.leigh.conley@gmail.com>
2016-06-24 14:30:19 -0400

    gave the states homes again

--- nc/views.py
+++ nc/views.py
@@ -4,6 +4,18 @@ from .models import Stop, Agency, Person
 from . import forms
 
 
+def home(request):
+    if request.method == 'GET' and request.GET:
+        form = forms.AgencySearchForm(request.GET)
+        if form.is_valid():
+            agency = form.cleaned_data['agency']
+            return redirect('agency-detail', agency.pk)
+    else:
+        form = forms.AgencySearchForm()
+    context = {'agency_form': form}
+    return render(request, 'nc.html', context)
+
+
 def search(request):
     query = None
     if request.method == 'GET' and request.GET:
