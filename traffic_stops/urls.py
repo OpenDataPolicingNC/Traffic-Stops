ce5688a06bd6dc277dbfc167535e5d344928b1d1
Neil Ashton <neil.m.ashton@gmail.com>
2016-06-24 15:29:15 -0400

    Namespacing URLs

--- traffic_stops/urls.py
+++ traffic_stops/urls.py
@@ -13,9 +13,8 @@ urlpatterns = [  # noqa
     url(r'^$', HomeView.as_view(), name='home'),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^selectable/', include('selectable.urls')),
-    url(r'^nc/', include('nc.urls')),
-    url(r'^md/', include('md.urls')),
-    url(r'^', include('md.urls')),
+    url(r'^nc/', include('nc.urls', namespace='nc')),
+    url(r'^md/', include('md.urls', namespace='md')),
     url(r'^about$', About.as_view(), name='about'),
     url(r'^update-session/', UpdateSession.as_view(), name='update_session'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
