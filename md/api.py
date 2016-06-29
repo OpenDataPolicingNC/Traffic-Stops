1281256377975d6c6f835ade98c6cbd1a5b71e58
Rebecca Conley <rebecca.leigh.conley@gmail.com>
2016-06-29 15:06:36 -0400

    modify gitignore, removed unused import

--- md/api.py
+++ md/api.py
@@ -1,5 +1,5 @@
 from django.db import connections
-from django.db.models import Count, Q
+from django.db.models import Count
 
 from rest_framework import viewsets
 from rest_framework.decorators import detail_route
