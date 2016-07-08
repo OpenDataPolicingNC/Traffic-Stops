9f9495552c6e302b9e8b6747ec7eddf12cd0cfc9
Neil Ashton <neil.m.ashton@gmail.com>
2016-07-08 13:41:08 -0400

    Adding LikelihoodOfSearch graphs to MD

--- traffic_stops/static/js/app/states/md/LikelihoodOfSearch.js
+++ traffic_stops/static/js/app/states/md/LikelihoodOfSearch.js
@@ -11,39 +11,37 @@ import $ from 'jquery';
 Backbone.$ = $;
 
 var LikelihoodSearchHandler = DataHandlerBase.extend({
-  clean_data: function(){
+  clean_data: function () {
 
     var years,
         raw = this.get("raw_data");
 
     // get available years
-    years = d3.set(raw.stops.map(function(v){return v.year;})).values();
-    years.filter(function(v){return (v >= Stops.start_year);});
+    years = d3.set(raw.stops.map((v) => v.year)).values();
+    years.filter((v) => (v >= Stops.start_year));
     years.push("Total");
 
     // get total searches/stops for all years by purpose
-    var getTotals = function(arr){
+    var getTotals = (arr) => {
       // calculate total for all years by purpose; push to array
       var purposes = d3.nest()
-                       .key(function(d) { return d.purpose; })
+                       .key((d) => d.purpose)
                        .entries(arr);
 
-      purposes.forEach(function(v){
-        // create new totals object, and reset race/ethnicity-values
+      purposes.forEach((v) => {
+        // create new totals object, and reset ethnicity-values
         var total = _.clone(v.values[0]);
 
-        _.keys(total).forEach(function(key){
-          if ((Stops.races.indexOf(key)>=0) ||
-              (Stops.ethnicities.indexOf(key)>=0)) {
+        _.keys(total).forEach((key) => {
+          if (Stops.ethnicities.indexOf(key) >= 0) {
             total[key] = 0;
           }
         });
 
         // sum data from all years
-        v.values.forEach(function(year){
-          _.keys(year).forEach(function(key){
-            if ((Stops.races.indexOf(key)>=0) ||
-                (Stops.ethnicities.indexOf(key)>=0)) {
+        v.values.forEach((year) => {
+          _.keys(year).forEach((key) => {
+            if (Stops.ethnicities.indexOf(key) >= 0) {
               total[key] += year[key];
             }
           });
@@ -53,8 +51,8 @@ var LikelihoodSearchHandler = DataHandlerBase.extend({
       });
     };
 
-    if (raw.stops.length>0) getTotals(raw.stops);
-    if (raw.searches.length>0) getTotals(raw.searches);
+    if (raw.stops.length > 0) getTotals(raw.stops);
+    if (raw.searches.length > 0) getTotals(raw.searches);
 
     // set cleaned-data to handler
     this.set("data", {
@@ -70,10 +68,11 @@ var LikelihoodOfSearch = VisualBase.extend({
     width: 750,
     height: 375
   },
-  setDefaultChart: function(){
+
+  setDefaultChart: function () {
     this.chart = nv.models.multiBarHorizontalChart()
-      .x(function(d){ return d.label; })
-      .y(function(d){ return d.value; })
+      .x((d) => d.label)
+      .y((d) => d.value)
       .width(this.get("width"))
       .height(this.get("height"))
       .margin({top: 20, right: 50, bottom: 20, left: 180})
@@ -88,7 +87,8 @@ var LikelihoodOfSearch = VisualBase.extend({
 
     this.chart.valueFormat(d3.format('%'));
   },
-  drawStartup: function(){
+
+  drawStartup: function () {
     // get year options for pulldown menu
     var selector = $('<select>'),
         year_options = this.data.years,
@@ -112,7 +112,8 @@ var LikelihoodOfSearch = VisualBase.extend({
 
     this.selector = selector;
   },
-  drawChart: function(){
+
+  drawChart: function () {
 
     d3.select(this.svg[0])
       .datum(this.dataset)
@@ -124,43 +125,44 @@ var LikelihoodOfSearch = VisualBase.extend({
 
     nv.utils.windowResize(this.chart.update);
   },
-  _getDataset: function(year){
+
+  _getDataset: function (year) {
     var dataset = [],
         raw = this.data.raw,
-        stops_arr = raw.stops.filter(function(v){return v.year===year;}),
-        searches_arr = raw.searches.filter(function(v){return v.year===year;}),
+        stops_arr = raw.stops.filter((v) => v.year===year),
+        searches_arr = raw.searches.filter((v) => v.year===year),
         stops = d3.map(),
         searches = d3.map(),
-        items = (this.get('showEthnicity')) ? Stops.ethnicities : Stops.races,
-        base = (this.get('showEthnicity')) ? "non-hispanic" : "white",
-        defRace = (this.get('showEthnicity')) ? "hispanic" : "black",
-        baseUpper = function(d){return d.charAt(0).toUpperCase() + d.slice(1);}(base);
+        items = Stops.ethnicities,
+        base = "White",
+        defRace = "Black",
+        baseUpper = ((d) => d.charAt(0).toUpperCase() + d.slice(1))(base);
 
     // turn arrays into maps with purpose as the key
-    stops_arr.forEach(function(v){
+    stops_arr.forEach((v) => {
       stops.set(v.purpose, v);
     });
-    searches_arr.forEach(function(v){
+    searches_arr.forEach((v) => {
       searches.set(v.purpose, v);
     });
 
-    // build a set of bars for each race, except for base
-    items.forEach(function(race, i){
-      if(race === base) return;
+    // build a set of bars for each ethnicity, except for base
+    items.forEach((ethnicity, i) => {
+      if (ethnicity === base) return;
 
       var bar = {
           color: Stops.colors[i],
-          key: `${Stops.pprint.get(race)} vs. ${baseUpper}`,
+          key: `${ethnicity} vs. ${baseUpper}`,
           values: [],
-          disabled: (race !== defRace)
+          disabled: (ethnicity !== defRace)
       };
 
       // build a bar for each violation
-      Stops.purpose_order.forEach(function(purpose){
+      Stops.purpose_order.forEach((purpose) => {
         // optional reporting requirement; remove as it's generally unreported
         if (purpose === "Checkpoint") return;
 
-        // calculate percent-difference of stops which led to searches by race,
+        // calculate percent-difference of stops which led to searches by ethnicity,
         // in comparison to base-baseline
         var search = searches.get(purpose),
             stop  = stops.get(purpose);
@@ -170,8 +172,8 @@ var LikelihoodOfSearch = VisualBase.extend({
           var rate, base_rate, r_rate,
               base_se = search[base] || 0,
               base_st = stop[base] || 0,
-              r_se = search[race] || 0,
-              r_st = stop[race] || 0;
+              r_se = search[ethnicity] || 0,
+              r_st = stop[ethnicity] || 0;
 
           base_rate = base_se/base_st;
           r_rate = r_se/r_st;
@@ -187,54 +189,48 @@ var LikelihoodOfSearch = VisualBase.extend({
         }
       });
 
-      // sort bars and then push race to list
+      // sort bars and then push ethnicity to list
       bar.values.sort(function(a,b){return a.order-b.order;});
       dataset.push(bar);
     });
 
     return dataset;
   },
-  triggerRaceToggle: function(e, v){
-    this.set('showEthnicity', v);
-    this.selector.trigger('change');
-  }
+
+  triggerRaceToggle: () => null
 });
 
 var LikelihoodSearchTable = TableBase.extend({
-  get_tabular_data: function(){
+  get_tabular_data: function () {
     var header, row, rows = [];
 
     // create header
     header = ["Year", "Stop-reason"];
-    header.push.apply(header, Stops.pprint.values());
+    header.push.apply(header, Stops.ethnicities);
     rows.push(header);
 
     var stop, search, stop_purp, search_purp, v1, v2,
         purposes = Stops.purpose_order.keys(),
         stops = this.data.raw.stops,
         searches = this.data.raw.searches,
-        get_row = function(stops, searches, term){
+        get_row = (stops, searches, term) => {
           var stop = (stops !== undefined) ? stops[term] : 0,
               search = (searches !== undefined) ? searches[term] : 0;
           return `${search}/${stop}`;
         };
 
     // create data rows
-    this.data.years.forEach(function(yr){
-        stop = stops.filter(function(d){return d.year == yr;});
-        search = searches.filter(function(d){return d.year == yr;});
-        purposes.forEach(function(purp){
+    this.data.years.forEach((yr) => {
+        stop = stops.filter((d) => d.year == yr);
+        search = searches.filter((d) => d.year == yr);
+        purposes.forEach((purp) => {
           row = [yr, purp];
-          stop_purp = (stop.length>0) ? stop.filter(function(d){return d.purpose == purp;}): undefined;
-          search_purp = (search.length>0) ? search.filter(function(d){return d.purpose == purp;}) : undefined;
+          stop_purp = (stop.length>0) ? stop.filter((d) => d.purpose == purp): undefined;
+          search_purp = (search.length>0) ? search.filter((d) => d.purpose == purp) : undefined;
           stop_purp = (stop_purp && stop_purp.length === 1) ? stop_purp[0] : undefined;
           search_purp = (search_purp && search_purp.length === 1) ? search_purp[0] : undefined;
 
-          Stops.races.forEach(function(r){
-            row.push(get_row(stop_purp, search_purp, r));
-          });
-
-          Stops.ethnicities.forEach(function(e){
+          Stops.ethnicities.forEach((e) => {
             row.push(get_row(stop_purp, search_purp, e));
           });
 
--- traffic_stops/static/js/app/states/md/index.js
+++ traffic_stops/static/js/app/states/md/index.js
@@ -1,9 +1,11 @@
 import Stops from './Stops.js';
 import Search from './Search.js';
+import LikelihoodOfSearch from './LikelihoodOfSearch.js';
 
 if (typeof window.MD === 'undefined') window.MD = {};
 
 Object.assign(window.MD,
   Stops,
-  Search
+  Search,
+  LikelihoodOfSearch
 )
