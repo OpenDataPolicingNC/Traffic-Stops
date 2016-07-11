15e882199838ed249d5f4f92902daff172491ade
Neil Ashton <neil.m.ashton@gmail.com>
2016-07-11 10:22:46 -0400

    Refactoring Search

--- traffic_stops/static/js/app/states/nc/Search.js
+++ traffic_stops/static/js/app/states/nc/Search.js
@@ -1,13 +1,7 @@
 import { StopsHandler, StopRatioDonut, StopRatioTimeSeries, StopsTable } from './Stops.js';
+import * as C from '../../common/Search.js';
 
-var SearchHandler = StopsHandler.extend({
-  clean_data: function(){
-    SearchHandler.__super__.clean_data.call(this);
-    var data = this.get("data");
-    data.type = "search";
-    this.set("data", data);
-  }
-});
+var SearchHandler = C.createSearchHandlerBase(StopsHandler);
 
 var SearchRatioDonut = StopRatioDonut.extend({});
 var SearchRatioTimeSeries = StopRatioTimeSeries.extend({});
