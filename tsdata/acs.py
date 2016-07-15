d52a5a21286133a756645fc78002a5db204897b3
Colin Copeland <copelco@caktusgroup.com>
2016-07-15 13:09:58 -0400

    add ACS Places; refactor into classes for DRY

--- tsdata/acs.py
+++ tsdata/acs.py
@@ -4,6 +4,11 @@
 # Notes:
 #  - Examples: http://api.census.gov/data/2014/acs5/examples.html
 #  - Places: https://www.census.gov/content/dam/Census/data/developers/understandingplace.pdf
+#
+# Fact Finder:
+#  - Home: http://factfinder.census.gov/faces/nav/jsf/pages/index.xhtml
+#  - Baltimore: http://factfinder.census.gov/bkmk/cf/1.0/en/county/Baltimore city, Maryland/RACE_AND_HISPANIC_ORIGIN
+#  - Baltimore Races: http://factfinder.census.gov/bkmk/table/1.0/en/ACS/14_5YR/DP05/0500000US24510
 
 import census
 import pandas as pd
@@ -17,71 +22,85 @@ RACE_VARIABLES = {
     'C02003_004E': 'black',
     'C02003_005E': 'native_american',
     'C02003_006E': 'asian',
-    'B03002_012E': 'hispanic',  # TODO: need to verify - maybe B03002_012E?
-    'C02003_008E': 'other',  # TODO: need to verify
+    'C02003_007E': 'native_hawaiian',
+    'C02003_008E': 'other',
+    'C02003_009E': 'two_or_more_races',
+    'B03002_012E': 'hispanic',
+    'B03002_002E': 'non_hispanic',
 }
 VARIABLES = ['NAME'] + list(RACE_VARIABLES.keys())  # NAME = geography/location
 
 
 def get_state_census_data(key, state_abbr):
     """Download several Census endpoints into a single DataFrame"""
-    counties = get_county_populations(key, state_abbr)
-    subdivs = get_subdivision_populations(key, state_abbr)
+    counties = ACSStateCounties(key, state_abbr).get()
+    subdivs = ACSStatePlaces(key, state_abbr).get()
     df = pd.concat([counties, subdivs])
-    # add state column
-    df['state'] = state_abbr
-    # convert race columns to numeric
-    num_cols = list(RACE_VARIABLES.values())
-    df[num_cols] = df[num_cols].apply(pd.to_numeric)
     return df
 
 
-def get_county_populations(key, state_abbr):
+class ACS(object):
+    """Base class to call ACS API and normalize output"""
+
+    source = "ACS 5-Year Data (2010-2014)"
+    geography = None
+    unique_ids = None
+    drop_columns = None
+
+    def __init__(self, key, state_abbr):
+        self.api = census.Census(key)
+        self.fips = getattr(states, state_abbr).fips
+        self.state_abbr = state_abbr
+
+    def call_api(self):
+        raise NotImplemented()
+
+    def get(self):
+        # load response (list of dicts) into pandas
+        df = pd.DataFrame(self.call_api())
+        # insert metadata
+        df['state'] = self.state_abbr
+        df['source'] = self.source
+        df['geography'] = self.geography
+        # add unique id
+        df['id'] = df.apply(lambda x: ''.join([getattr(x, col) for col in self.unique_ids]),  # noqa
+                            axis=1)
+        # rename common columns
+        df.rename(columns={'NAME': 'location'}, inplace=True)
+        # replace census variable names with easier to read race labels
+        df.rename(columns=RACE_VARIABLES, inplace=True)
+        # convert race columns to numerics
+        num_cols = list(RACE_VARIABLES.values())
+        df[num_cols] = df[num_cols].apply(pd.to_numeric)
+        # remove unused columns
+        if self.drop_columns:
+            df.drop(self.drop_columns, axis=1, inplace=True)
+        return df
+
+
+class ACSStateCounties(ACS):
     """
-    Return list of state's counties.
+    State County Demographics
     ex: http://api.census.gov/data/2014/acs5?get=NAME&for=county:*&in=state:24
     code: https://github.com/sunlightlabs/census/blob/master/census/core.py#L181
     """
-    # request census data
-    c = census.Census(key)
-    fips = getattr(states, state_abbr).fips
-    data = c.acs.state_county(VARIABLES, fips, census.ALL)
-    # load response (list of dicts) into pandas
-    df = pd.DataFrame(data)
-    # rename columns to include race labels
-    df.rename(columns={'NAME': 'location'}, inplace=True)
-    df.rename(columns=RACE_VARIABLES, inplace=True)
-    # create unique id column
-    df['id'] = df.apply(lambda x: x['state'] + x['county'], axis=1)
-    # add source info
-    df['source'] = "ACS 5-Year Data (2010-2014)"
-    df['type'] = 'County'
-    # remove unused columns
-    df.drop(['county', 'state'], axis=1, inplace=True)
-    return df
+    unique_ids = ('state', 'county')
+    geography = 'county'
+    drop_columns = ['county']
+
+    def call_api(self):
+        return self.api.acs.state_county(VARIABLES, self.fips, census.ALL)
 
 
-def get_subdivision_populations(key, state_abbr):
+class ACSStatePlaces(ACS):
     """
-    Return list of state's county subdivisions.
-    ex: http://api.census.gov/data/2014/acs5?get=NAME&for=county+subdivision:*&in=state:24
+    State Place Demographics
+    ex: http://api.census.gov/data/2014/acs5?get=NAME&for=place:*&in=state:24
+    code: https://github.com/sunlightlabs/census/blob/master/census/core.py#L215
     """
-    # request census data
-    c = census.Census(key)
-    fips = getattr(states, state_abbr).fips
-    geo = {'for': 'county subdivision:{}'.format(census.ALL),
-           'in': 'state:{}'.format(fips)}
-    data = c.acs.get(VARIABLES, geo)
-    # load response (list of dicts) into pandas
-    df = pd.DataFrame(data)
-    # rename columns to include race labels
-    df.rename(columns={'NAME': 'location'}, inplace=True)
-    df.rename(columns=RACE_VARIABLES, inplace=True)
-    # create unique id column
-    df['id'] = df.apply(lambda x: x['state'] + x['county'] + x['county subdivision'], axis=1)
-    # add static info
-    df['source'] = "ACS 5-Year Data (2010-2014)"
-    df['type'] = 'County Subdivision'
-    # remove unused columns
-    df.drop(['county', 'state', 'county subdivision'], axis=1, inplace=True)
-    return df
+    unique_ids = ('state', 'place')
+    geography = 'place'
+    drop_columns = ['place']
+
+    def call_api(self):
+        return self.api.acs.state_place(VARIABLES, self.fips, census.ALL)
