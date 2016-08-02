0c0c16031116ea778cb761e1fed6f6733ab8bd16
Neil Ashton <neil.m.ashton@gmail.com>
2016-08-02 12:05:35 -0400

    More CBV-y implementation of searches

--- md/urls.py
+++ md/urls.py
@@ -8,7 +8,7 @@ router.register(r'agency', api.AgencyViewSet, base_name="agency-api")
 
 
 urlpatterns = [  # noqa
-    url(r'^$', views.home, name='home'),
+    url(r'^$', views.Home.as_view(), name='home'),
     url(r'^search/$', views.search, name='stops-search'),
     url(r'^agency/$', views.AgencyList.as_view(), name='agency-list'),
     url(r'^agency/(?P<pk>\d+)/$', views.AgencyDetail.as_view(),
