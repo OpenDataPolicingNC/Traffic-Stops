3bafd34a253022917aac3f4417b29bc586f0b9f1
Rebecca Conley <rebecca.leigh.conley@gmail.com>
2016-08-02 11:55:41 -0400

    add basic valid form test

--- nc/forms.py
+++ nc/forms.py
@@ -55,6 +55,7 @@ class SearchForm(forms.Form):
 
     def clean(self):
         cleaned_data = super(SearchForm, self).clean()
+
         start_date = cleaned_data.get('start_date')
         end_date = cleaned_data.get('end_date')
         if start_date is not None and end_date is not None:
