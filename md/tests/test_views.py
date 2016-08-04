c2f374064745715865ac402cf431f3ae4681ec51
Neil Ashton <neil.m.ashton@gmail.com>
2016-08-04 12:57:55 -0400

    Rebase

--- md/tests/test_views.py
+++ md/tests/test_views.py
@@ -1,6 +1,7 @@
 from django.core.urlresolvers import reverse
-from django.test import TestCase
+from django.test import TestCase, RequestFactory
 from md.tests import factories
+from .. import views
 
 
 class ViewTests(TestCase):
@@ -59,3 +60,35 @@ class ViewTests(TestCase):
             self.assertEqual(2, len(chunks[0]))
             self.assertEqual(2, len(chunks[1]))
             self.assertEqual(1, len(chunks[2]))
+
+
+class TestSearchView(TestCase):
+
+    def test_search_good_data(self):
+        self.factory = RequestFactory()
+        request = self.factory.get('/md/search', data={
+            'agency': 'Montgomery County Police Department',
+        })
+        response = views.search(request)
+        text = 'error'
+        self.assertNotContains(response, text, status_code=200)
+
+    def test_search_bad_purpose_error(self):
+        self.factory = RequestFactory()
+        request = self.factory.get('/md/search', data={
+            'agency': 'Montgomery County Police Department',
+            'purpose': [25]
+        })
+        response = views.search(request)
+        text = 'Select a valid choice. 25 is not one of the available choices'
+        self.assertContains(response, text, status_code=200)
+
+    def test_search_bad_gender_error(self):
+        self.factory = RequestFactory()
+        request = self.factory.get('/md/search', data={
+            'agency': 'Montgomery County Police Department',
+            'gender': 'hippopotamus'
+        })
+        response = views.search(request)
+        text = 'Stops (0 total)'
+        self.assertContains(response, text, status_code=200)
