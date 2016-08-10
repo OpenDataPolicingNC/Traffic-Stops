d47f36e7ff13b0532344b2b858c4e2be48887e8d
Jeff Trawick <trawick@gmail.com>
2016-08-10 11:25:51 -0400

    Fix argparse usage

--- nc/management/commands/prime_cache.py
+++ nc/management/commands/prime_cache.py
@@ -7,7 +7,7 @@ class Command(BaseCommand):
     """Prime cache on production server"""
 
     def add_arguments(self, parser):
-        parser.add_argument('url', default="http://0.0.0.0:8000/")
+        parser.add_argument('url', nargs='?', default="http://0.0.0.0:8000/")
 
     def handle(self, *args, **options):
         prime_cache.run(options['url'])
