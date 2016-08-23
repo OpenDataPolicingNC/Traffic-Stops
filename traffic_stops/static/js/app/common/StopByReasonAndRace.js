054af7a5dd281c54ad138a76d3f82b15ea3811c3
Neil Ashton <neil.m.ashton@gmail.com>
2016-08-23 08:45:23 -0400

    Adding `common` file for StopByReasonAndRaceBase

---
+++ traffic_stops/static/js/app/common/StopByReasonAndRace.js
@@ -0,0 +1,8 @@
+import _ from 'underscore';
+import d3 from 'd3';
+
+import VisualBase from '../base/VisualBase.js';
+import TableBase from '../base/TableBase.js';
+
+export const StopByReasonAndRaceBase = VisualBase.extend({});
+export const StopByReasonAndRaceTableBase = TableBase.extend({});
