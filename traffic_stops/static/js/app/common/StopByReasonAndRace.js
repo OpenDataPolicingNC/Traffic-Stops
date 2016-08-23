f78eda39d0a7d296d5eae4c06b2e3e9574a170b2
Neil Ashton <neil.m.ashton@gmail.com>
2016-08-23 13:05:38 -0400

    Adding line graph

--- traffic_stops/static/js/app/common/StopByReasonAndRace.js
+++ traffic_stops/static/js/app/common/StopByReasonAndRace.js
@@ -5,6 +5,10 @@ import VisualBase from '../base/VisualBase.js';
 import TableBase from '../base/TableBase.js';
 
 export const SRRTimeSeriesBase = VisualBase.extend({
+  Stops: { }, // abstract property, requires override
+  _items: function () { throw "abstract method: requires override"; },
+  _pprint: function () { throw "abstract method: requires override"; },
+
   setDefaultChart: function () {
     this.chart = nv.models.lineChart()
                   .useInteractiveGuideline (true)
@@ -12,7 +16,6 @@ export const SRRTimeSeriesBase = VisualBase.extend({
                   .showLegend(true)
                   .showYAxis(true)
                   .showXAxis(true)
-                  .forceY([0, 1])
                   .width(this.get("width"))
                   .height(this.get("height"));
 
@@ -22,25 +25,38 @@ export const SRRTimeSeriesBase = VisualBase.extend({
 
     this.chart.yAxis
         .axisLabel('Number of stops by race')
-        .tickFormat(d3.format('%'));
+        .tickFormat(d3.format('.0d'));
   },
 
   drawStartup: function () {
     var $selector = $('<select>');
     var purposes = d3.set(_.pluck(this.data.raw.stops, 'purpose'));
-    var $opts = purposes.values().map((p) => $(`<option value="${p}">${p}</option>`));
+    var $opts = [$('<option value="All">All</option>')].concat(
+      purposes
+        .values()
+        .sort()
+        .map((p) => $(`<option value="${p}">${p}</option>`))
+    )
+    var update = () => {
+      var val = $selector.val() || 'All';
+      this.dataset = this._getByPurpose(val);
+      this.drawChart();
+    }
 
     $selector
       .append($opts)
-      .val('Seat Belt Violation');
+      .val('All')
+      .on('change', update);
 
     $('<div class="selector-container">')
       .html($selector)
       .appendTo(this.div);
+
+    update();
   },
 
   drawChart: function () {
-    var data = this._formatData();
+    var data = this.dataset;
 
     nv.addGraph(() => {
         d3.select(this.svg[0])
@@ -55,9 +71,88 @@ export const SRRTimeSeriesBase = VisualBase.extend({
       });
   },
 
-  _formatData: function () {
-    return [];
-  }
+  _years: _.memoize(function () {
+    var years_all = d3.set(_.pluck(this.data.raw.stops, 'year')).values();
+    var years = _.without(years_all, 'Total');
+    return years;
+  }),
+
+  _getByPurpose: _.memoize(function (purpose) {
+    var types = this._items();
+    var years = this._years();
+    var raw_data, data;
+
+    if (purpose === 'All') {
+      raw_data = this._purposeAll();
+    } else {
+      raw_data = this.data.raw.stops;
+    }
+
+    var data = types.map((type, i) => ({
+      key: this._pprint(type)
+    , values: years.map((year) => ({
+        x: year
+      , y: (_.find(raw_data, (stop) => (
+          String(stop.year) === year
+            && stop.purpose === purpose
+        )) || {})[type] || 0
+    }))
+    , color: this.Stops.colors[i]
+    }));
+
+    return this._checkThreshold(data);
+  }),
+
+  _checkThreshold: function (data_) {
+    var data = _.clone(data_);
+    var values = _.flatten(data.map((type) => (type.values.map((value) => value.y))));
+    var overall_max = d3.max(values);
+
+    data.forEach((datum) => {
+      var local_max = d3.max(datum.values.map((v) => v.y));
+      if ((local_max / overall_max) < 0.1) {
+        datum.disabled = true;
+      }
+    });
+
+    return data;
+  },
+
+  _purposeAll: _.memoize(function () {
+    var data = [];
+    var years = this._years();
+
+    years.forEach((year) => {
+      var totals = {
+        year
+      , purpose: 'All'
+      };
+
+      var stops = _.filter(
+        this.data.raw.stops
+      , (stop) => (String(stop.year) === year)
+      );
+
+      stops.forEach((stop) => {
+        var keys = _.chain(stop)
+          .keys()
+          .without('year', 'purpose').
+          value();
+
+        keys.forEach((k) => {
+          if (typeof totals[k] === 'undefined') {
+            totals[k] = 0;
+          }
+
+          totals[k] += stop[k];
+        });
+      });
+
+      data.push(totals);
+    });
+
+    return data;
+  })
 });
 
 export const SRRTableBase = TableBase.extend({});
