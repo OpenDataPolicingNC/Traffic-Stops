79b3017fd7838697286fb4eb405d36e64ca0057e
Neil Ashton <neil.m.ashton@gmail.com>
2016-08-25 08:41:56 -0400

    Adding DataHandlerBase tests

--- traffic_stops/static/js/test/base/test_DataHandlerBase.js
+++ traffic_stops/static/js/test/base/test_DataHandlerBase.js
@@ -6,6 +6,82 @@ import Backbone from 'backbone'
 describe('base', () => {
   describe('DataHandlerBase', () => {
     describe('get_data', () => {
+      it('returns a Promise', () => {
+        DHB.__Rewire__('d3', {
+          json: (url, cb) => {
+            cb(undefined, 'data')
+          }
+        })
+
+        let DataHandlerBase_ = DataHandlerBase.extend({
+          clean_data: () => true
+        })
+
+        let dhb = new DataHandlerBase_({url: 'foo'})
+        let result = dhb.get_data()
+
+        assert.isTrue(result instanceof Promise)
+
+        DHB.__ResetDependency__('d3')
+      });
+
+      it('resolves its Promise on fetch success', (done) => {
+        let DataHandlerBase_ = DataHandlerBase.extend({
+          clean_data: () => true
+        })
+
+        DHB.__Rewire__('d3', {
+          json: (url, cb) => {
+            cb(undefined, 'data')
+          }
+        })
+
+        let dhb = new DataHandlerBase_({url: 'foo'})
+        dhb.get_data().then(() => {
+          DHB.__ResetDependency__('d3')
+          done()
+        })
+      })
+
+      it('rejects its Promise on fetch fail', (done) => {
+        let DataHandlerBase_ = DataHandlerBase.extend({
+          clean_data: () => true
+        })
+
+        DHB.__Rewire__('d3', {
+          json: (url, cb) => {
+            cb('error', undefined)
+          }
+        })
+
+        let dhb = new DataHandlerBase_({url: 'foo'})
+        dhb.get_data().catch(() => {
+          DHB.__ResetDependency__('d3')
+          done()
+        })
+      })
+
+      it('resolves with its "data" attribute', (done) => {
+        let accept = 'accept'
+
+        let DataHandlerBase_ = DataHandlerBase.extend({
+          clean_data: function () { this.set('data', accept) }
+        })
+
+        DHB.__Rewire__('d3', {
+          json: (url, cb) => {
+            cb(undefined, true)
+          }
+        })
+
+        let dhb = new DataHandlerBase_({url: 'foo'})
+        dhb.get_data().then((data) => {
+          DHB.__ResetDependency__('d3')
+          assert.equal(accept, data)
+          done()
+        })
+      })
+
       it('calls d3.json on its own "url" property', (done) => {
         let urlAccept = 'foo'
 
