5a9bf9924e71e4ebb7820933391011b96008aa23
Kia Lam <digitalanime@gmail.com>
2018-01-24 11:37:53 -0500

    adjust Search charts to account for new ethnicities group

--- traffic_stops/static/js/app/states/nc/StopSearch.js
+++ traffic_stops/static/js/app/states/nc/StopSearch.js
@@ -4,36 +4,32 @@ import * as C from '../../common/StopSearch.js';
 const StopSearchHandler = C.StopSearchHandlerBase.extend({
   Stops: Stops,
 
-  major_type: Stops.races,
+  major_type: Stops.ethnicities,
 
-  types: [Stops.races, Stops.ethnicities],
+  types: [Stops.ethnicities],
 
   _pprint: (d) => Stops.pprint.get(d)
 });
 
 const StopSearchTimeSeries = C.StopSearchTimeSeriesBase.extend({
   defaults: {
-    showEthnicity: false,
     width: 750,
     height: 375
   },
 
   Stops: Stops,
 
-  defaultEnabled: ["Total", "White", "Black", "Hispanic", "Non-hispanic"],
+  defaultEnabled: ["Total", "White", "Black", "Hispanic"],
 
   _items: function () {
-    return (this.get('showEthnicity')) ? Stops.ethnicities : Stops.races;
+    return Stops.ethnicities;
   },
 
   _pprint: function (type) {
     return Stops.pprint.get(type);
   },
 
-  triggerRaceToggle: function(e, v){
-    this.set('showEthnicity', v);
-    this.drawChart();
-  }
+  triggerRaceToggle: () => null
 });
 
 var StopSearchTable = C.StopSearchTableBase.extend({});
