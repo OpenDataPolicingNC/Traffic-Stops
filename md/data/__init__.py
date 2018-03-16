2ad5b752d5cc99285030c79c71f15aaa18b407a4
Mark Lavin <mlavin@caktusgroup.com>
2018-03-16 14:22:30 -0400

    Update S3 URL.

--- md/data/__init__.py
+++ md/data/__init__.py
@@ -1,1 +1,1 @@
-DEFAULT_URL = 'https://s3-us-west-2.amazonaws.com/openpolicingdata/MD_Traffic_Stops_through_20161003.zip'  # noqa
+DEFAULT_URL = 'https://s3-us-west-2.amazonaws.com/openpolicingdata/md/merged/MD_Traffic_Stops_through_20171231.zip'  # noqa
